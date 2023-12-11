d598cfed7e3dc1a7df4ccd93d017c1d880734921
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-12-12 03:23:33 +0900

    first commit

---
+++ src/main/java/plugin/Main.java
@@ -0,0 +1,13 @@
+package plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+}
