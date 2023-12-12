03b0bc4ab96ee855d20eb1eb2de30c35f23a2459
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-12-12 16:28:43 +0900

    出現する敵の種類がランダムになるよう実装

--- src/main/java/plugin/command/EnemyDownCommand.java
+++ src/main/java/plugin/command/EnemyDownCommand.java
@@ -1,11 +1,13 @@
 package plugin.command;
 
+import java.util.List;
 import java.util.SplittableRandom;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Enemy;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
@@ -14,19 +16,45 @@ public class EnemyDownCommand implements CommandExecutor {
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof Player player) {
+      World world = player.getWorld();
+
       player.setHealth(20);
       player.setFoodLevel(20);
 
-      World world = player.getWorld();
-      Location playerLocation = player.getLocation();
-      double x = playerLocation.getX();
-      double y = playerLocation.getY();
-      double z = playerLocation.getZ();
-
-      int random = new SplittableRandom().nextInt(20) - 10;
-
-      world.spawnEntity(new Location(world, (x + random), y, (z + random)), EntityType.ZOMBIE);
+      world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
     }
     return false;
+  }
+
+  /**
+   * 敵の出現場所を取得します。
+   * 出現エリアはX軸とZ軸は自分の位置からプラス、ランダムで-10~9の値が設定されます。
+   * Y軸はプレイヤーと同じ位置になります。
+   *
+   * @param player コマンドを実行したプレイヤー
+   * @param world コマンドを実行したプレイヤーが所属するワールド　
+   * @return 敵の出現場所
+   */
+  private Location getEnemySpawnLocation(Player player, World world) {
+    Location playerLocation = player.getLocation();
+
+    int randomX = new SplittableRandom().nextInt(20) - 10;
+    int randomZ = new SplittableRandom().nextInt(20) - 10;
+
+    double x = playerLocation.getX() + randomX;
+    double y = playerLocation.getY();
+    double z = playerLocation.getZ() + randomZ;
+
+    return new Location(world, x, y, z);
+  }
+
+  /**
+   * ランダムで敵を抽選して、その結果の敵を取得します。
+   *
+   * @return 敵
+   */
+  private EntityType getEnemy() {
+    List<EntityType> enemyList = List.of(EntityType.ZOMBIE, EntityType.SKELETON);
+    return enemyList.get(new SplittableRandom().nextInt(2));
   }
 }
